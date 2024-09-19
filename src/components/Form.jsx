c57f8af7ca5e1cdc2b4e6f179891b5293da5eb9f
Naga Sri Venkat Rentala <venkatrentala2001@gmail.com>
2024-09-19 16:56:34 +0530

    Create a form page using mock api in react

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,171 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './Form.css';
 import { FaLongArrowAltDown } from "react-icons/fa";
 function Form() {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    phoneNumber: "",
+    email: "",
+    userName: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [errors, setErrors] = useState({
+    fullName: "",
+    phoneNumber: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (selectedUser) {
+      setFormData(selectedUser);
+    }
+  }, [selectedUser]);
+  const handleReset = () => {
+    setFormData({
+      fullName: "",
+      phoneNumber: "",
+      email: "",
+      userName: "",
+      password: "",
+      confirmPassword: "",
+    });
+    setErrors({
+      fullName: "",
+      phoneNumber: "",
+      email: "",
+      userName: "",
+      password: "",
+      confirmPassword: "",
+    });
+    setSelectedUser(null);
+  };
+  const handleBlur = (event) => {
+    const { name, value } = event.target;
+    let validateErrors = { ...errors };
+    if (!value) {
+      validateErrors[name] = `${
+        name.charAt(0).toUpperCase() + name.slice(1)
+      } is required `;
+    } else {
+      validateErrors[name] = "";
+    }
+    setErrors(validateErrors);
+  };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  let emailPattern =
+    /^([a-zA-Z0-9]+)@([a-zA-Z0-9-]+).([a-zA-Z]+).([a-zA-Z]{2,20})$/;
+  let upperCasePattern = /[A-Z]/;
+  let lowerCasePattern = /[a-z]/;
+  let numberPattern = /[0-9]/;
+  let specialCharacterPattern = /[~!@#%&()$^_?]/;
+  let minlengthCharacterPattern = /^.{8,16}$/;
+  let phonenumberPattern = /^([0-9]{10})$/;
+  const validateForm = () => {
+    let valid = true;
+    let newErrors = [];
+    if (!formData.fullName) {
+      newErrors.fullName = "Full Name is required";
+      valid = false;
+    } else {
+      newErrors.fullName = " ";
+    }
+    if (!formData.phoneNumber) {
+      newErrors.phoneNumber = "Mobile Number is required";
+      valid = false;
+    } else {
+      if (!phonenumberPattern.test(formData.phoneNumber)) {
+        newErrors.phoneNumber = "Mobile number should be 10 digits";
+        valid = false;
+      } else {
+        newErrors.phoneNumber = " ";
+      }
+    }
+    if (!formData.email) {
+      newErrors.email = "Email is required";
+      valid = false;
+    } else {
+      if (!emailPattern.test(formData.email)) {
+        newErrors.email = "Enter valid email";
+        valid = false;
+      } else {
+        newErrors.email = " ";
+      }
+    }
+    if (!formData.password) {
+      newErrors.password = "Password is required";
+      valid = false;
+    } else {
+      if (!minlengthCharacterPattern.test(formData.password)) {
+        newErrors.password = "password must be between 8 to 16 characters";
+        valid = false;
+      } else if (!lowerCasePattern.test(formData.password)) {
+        newErrors.password = "At least 1 lowercase character";
+        valid = false;
+      } else if (!numberPattern.test(formData.password)) {
+        newErrors.password = "At least 1 number";
+        valid = false;
+      } else if (!specialCharacterPattern.test(formData.password)) {
+        newErrors.password = "At least 1 special character";
+        valid = false;
+      } else if (!upperCasePattern.test(formData.password)) {
+        newErrors.password = "At least 1 uppercase character";
+        valid = false;
+      } else {
+        newErrors.password = " ";
+      }
+    }
+    if (!formData.confirmPassword) {
+      newErrors.confirmPassword = "Confirm password is required";
+      valid = false;
+    } else if (formData.password !== formData.confirmPassword) {
+      newErrors.confirmPassword =
+        "Check your confirm password and password should be same";
+      valid = false;
+    } else {
+      newErrors.confirmPassword = "";
+    }
+    setErrors(newErrors);
+    return valid;
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+      if (selectedUser) {
+        // Update existing user
+        setUsers(
+          users.map((user) =>
+            user.email === selectedUser.email ? formData : user
+          )
+        );
+        setSelectedUser(null); // Clear selection after update
+      } else {
+        // Add new user
+        setUsers([...users, formData]);
+      }
+      setFormData({
+        fullName: "",
+        phoneNumber: "",
+        email: "",
+        userName: "",
+        password: "",
+        confirmPassword: "",
+      });
+      setErrors({
+        fullName: "",
+        phoneNumber: "",
+        email: "",
+        userName: "",
+        password: "",
+        confirmPassword: "",
+      });
+      navigate("/users");
+    }
+  };
   return (
     <div className="booking">
       <div className="section-center">
